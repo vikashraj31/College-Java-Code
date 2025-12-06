d3a260728e9ec3371571b9d72b4f1e64f54001cd
vikashvanshi <vikashh3121@gmail.com>
2025-12-07 02:03:17 +0530

    push

--- src/FirstCode.java
+++ src/FirstCode.java
@@ -2,5 +2,6 @@ class FirstCode {
     static void main(String[] args) {
         // This line prints text on the screen
         System.out.println("First Java Program");
+        System.out.println("Hellow World");
     }
 }
