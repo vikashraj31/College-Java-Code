d590b324119687ed4b08c02cf67b42616ba514d0
vikashvanshi <vikashh3121@gmail.com>
2025-12-07 02:15:01 +0530

    New

---
+++ src/Info.java
@@ -0,0 +1,9 @@
+class Info {
+    public static void main(String[] args) {
+        System.out.println("Name: Vikash");
+        System.out.println("Course: B.Tech");
+        System.out.println("Branch: CSE");
+        System.out.println("College: KIIT University");
+        System.out.println("City: Bhubaneswar");
+    }
+}
